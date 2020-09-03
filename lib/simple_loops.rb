fc422153e7ff20fc13c2a61fac19aab23de09258
Tom ODay <tomoday91@gmail.com>
2020-09-03 01:09:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,8 +29,9 @@ start = [5, 4, 3, 2, 1]
 def return_string_array(array)
   count = 0
   while count < array.length do
-    array[count].to_s
+    array[count].to_s << new_array[]
     count += 1
+    return new_array
   end
 end
 
